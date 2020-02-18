e2a276e28a17d0813af062e0752066d5db56bd05
Lucaslpena <lucaslpena@gmail.com>
2020-02-18 22:25:42 +0100

    updating footer signature

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ export default class Footer extends Component {
     return(
       <footer>
 
-        <h1>W3}RD</h1>
+        <h1>w3}rd</h1>
         <Link href={'mailto:w3r@weird.fyi'} prefetch={false}>
           <a target={'_blank'}>
             <p>w3r@weird.fyi</p>
