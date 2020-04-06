d95eb49753b24e70601b9eb3374002d9921cd430
Lucaslpena <lucaslpena@gmail.com>
2020-04-06 11:56:58 +0200

    obj rotating

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,46 @@ import Head from 'next/head'
 import Normalize from 'react-normalize'
 import '../styles.scss'
 import { Nav, Footer } from '../components'
+import React, {Component } from 'react'
 
-const Home = () => {
-  return(
-    <div className="container">
-      <Normalize />
-      <Head>
-        <title>W3}RD — Futures Design Collective</title>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
-      <Nav/>
-      <main>
+import dynamic from 'next/dynamic';
 
-      </main>
+const P5Wrapper = dynamic(import('react-p5-wrapper'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 
-      <Footer />
-    </div>
-  )
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      width: 100,
+      height: 400
+    };
+  }
+
+  componentDidMount() {
+    this.setState({width: window.innerWidth, height: window.innerHeight});
+  }
+
+  render() {
+    const { width, height } = this.state;
+    const sketch = require('../sketches/sketch').default(width, height);
+
+    return (
+      <div className="container">
+        <Normalize/>
+        <Head>
+          <title>W3}RD — Futures Design Collective</title>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <Nav/>
+        <main>
+          <P5Wrapper className={'background-sketch'} sketch={sketch} />
+        </main>
+
+        <Footer/>
+      </div>
+    )
+  }
 }
-
-export default Home
---
+++ sketches/sketch.js
@@ -0,0 +1,94 @@
+export default (w, h) => (p) => {
+  let obj;
+  let mousePress = false;
+  let x, y, xspeed, yspeed;
+  let windowXMin, windowXMax, windowYMin, windowYMax;
+  let thetaX, thetaY, thetaZ;
+  let thetaDegrade = 0.1;
+
+  p.preload = () => {
+    obj = p.loadModel('assets/weird-object-old.obj', true);
+  }
+
+  p.setup = () => {
+    // p.frameRate(15);
+    p.createCanvas(w, h, p.WEBGL);
+    p.angleMode(p.RADIANS);
+
+    windowXMax = p.windowWidth * .3;
+    windowXMin = windowXMax * -1;
+
+    windowYMax = p.windowHeight * .3;
+    windowYMin = windowYMax * -1;
+
+    x = p.random(windowXMin, windowXMax);
+    y = p.random(windowYMin, windowYMax);
+
+    xspeed = 1;
+    yspeed = 1;
+
+    thetaX = 0;
+    thetaY = 0;
+    thetaZ = 0;
+
+
+  }
+
+  p.draw = () => {
+    p.background(255);
+    x += xspeed;
+    y += yspeed;
+
+    p.pointLight(0, 0, 255, 0, -200, 0);
+    p.pointLight(0, 255, 0, 0, 200, 0);
+    p.ambientLight(25);
+    p.push();
+
+    p.translate(x, y);
+    p.fill('#FF0000');
+    p.scale(3);
+
+    if (mousePress === true){
+      console.log('z-tate');
+      thetaY = p.frameCount * 0.03 ;
+    } else {
+      thetaY = p.frameCount * 0.01;
+    }
+    thetaX = p.frameCount * 0.01;
+    thetaZ = p.frameCount * 0.01;
+
+
+    p.rotateX(thetaX);
+    p.rotateY(thetaY);
+    p.rotateZ(thetaZ);
+    p.ambientMaterial(255);
+    p.noStroke();
+    p.model(obj);
+
+    p.pop();
+
+    if ( (x + 100 <= windowXMin) || (x + 100 >= windowXMax)){
+      xspeed *= -1;
+    }
+    if ( (y + 100 <= windowYMin) || (y + 100 >= windowYMax)) {
+      yspeed *= -1;
+    }
+
+  }
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  }
+
+  p.mouseClicked = () => {
+    if (mousePress === false) {
+      mousePress = true;
+      console.log('pressed');
+      setTimeout(() => {
+        console.log('done');
+        mousePress = false;
+
+      }, 750);
+    }
+  }
+}
